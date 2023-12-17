555b3007871392ea504be753c4991cb8322d29ef
punithraj97 <142416431+punithraj97@users.noreply.github.com>
2023-12-17 10:50:07 -0500

    Update header.h

--- header.h
+++ header.h
@@ -3,6 +3,29 @@
 
 typedef struct _node_
 {
+    char name[MAX];
+    int age;
+    float weight;
+    float height;
+    int bmi;
     struct _node_ *ptr;
-}
+}CANDIDATE;
+typedef struct _batch_
+{
+   CANDIDATE *head,*tail;
+   int count;
+}BATCH;
 
+typedef struct _overweight_
+{  
+   int obess;
+   int under_weight;
+} OVERWEIGHT;
+   
+BATCH* initialization();
+OVERWEIGHT* initialization_overweight();
+CANDIDATE* getnode(char*,int,float,float);
+int insert(BATCH*,char*,int,float,float);
+int calculate_underweight(BATCH*,OVERWEIGHT*);
+void display(BATCH*);
+   
